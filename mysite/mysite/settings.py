3b3fa94c0c9616de0337bc5223209a851b193133
Evgeny Ponomarev <evgn_p@mail.ru>
2022-12-05 23:41:13 +0300

    finally

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-owg5(7r0&5z6o2&*qalg@#lm3j8^)&^ce^uhdt2j)$f5!p!v)4'
+SECRET_KEY = SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
 ALLOWED_HOSTS = []
 
